9e88cd8732df7cb67ea1d6adb9760d0063afdf40
mungai michael <mungaimikey@gmail.com>
2024-02-11 14:51:56 +0300

    fixed the data fetch hook

--- Hooks/useDataFetch.js
+++ Hooks/useDataFetch.js
@@ -4,31 +4,49 @@ import { useEffect, useState } from "react";
 const useDataFetch = (url) => {
 
 
-    const [houses, setHouses] = useState([]);
+    const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         const fetchData = async () => {
+            const options = {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                    'X-RapidAPI-Key': '8529d88d8cmsh282aa4bd8f5e139p159607jsn18f9942111cd',
+                    'X-RapidAPI-Host': 'realty-in-us.p.rapidapi.com'
+                },
+                body: JSON.stringify({
+
+                    limit: 10,
+                    offset: 0,
+                    postal_code: '90004',
+                    status: ['for_sale'],
+                    sort: {
+                        direction: 'desc',
+                        field: 'list_date'
+                    }
+                })
+            };
+
             try {
-                const response = await fetch(url);
+                const response = await fetch(url, options);
                 setLoading(true)
-                if (response.ok) {
-                    const data = await response.json();
-                    setHouses(data);
-                    setLoading(false)
-                } else {
-                    throw new Error('Failed to fetch data');
-                }
+                const data = await response.json()
+                setData(data.data.home_search.results)
+                setLoading(false)
             } catch (error) {
-                console.error('Error fetching data:', error);
+                console.error(error);
             }
+
         };
+
 
         fetchData();
     }, []);
 
 
-    return { houses, loading }
+    return { data, loading }
 }
 
 export default useDataFetch; 
