dfa81956c7374f7d0aed2eb63ea919dfb8b8bd36
mungai michael <mungaimikey@gmail.com>
2024-02-11 14:52:39 +0300

    added custom fonts

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar, View } from 'react-native';
 import Navigator from './Navigator';
 import { NavigationContainer } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+
+import * as SplashScreen from "expo-splash-screen"
+
+import { useCallback } from 'react';
 
 
 
 export default function App() {
+
+  const [fontsLoaded, fontError] = useFonts({
+    'bold': require('./assets/fonts/Quicksand-Bold.ttf'),
+    'light': require('./assets/fonts/Quicksand-Light.ttf'),
+    'regular': require('./assets/fonts/Quicksand-Regular.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View className="flex flex-1 "  >
       <StatusBar barStyle={"dark-content"} />
