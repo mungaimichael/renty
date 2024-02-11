57794bb476a6df2c956e800785fa2bb2eff70404
mungai michael <mungaimikey@gmail.com>
2024-02-11 20:44:51 +0300

    added Stack screen to allow further navigation

---
+++ Screens/StackScreens/index.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+
+    // const Stack = createStac
+    return (
+        <View>
+            <Text>index</Text>
+        </View>
+    )
+}
+
+export default index
